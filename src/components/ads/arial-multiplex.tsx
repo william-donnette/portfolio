1872f6c8d81b0c209c3dc8fa492fb02c71068a49
william-donnette <willdonn17@gmail.com>
2025-02-09 00:54:27 +0100

    feat: :rocket: Introduce Multiplex Ads

---
+++ src/components/ads/arial-multiplex.tsx
@@ -0,0 +1,14 @@
+import {FunctionComponent} from 'react';
+
+interface ArialMultiplexProps {}
+
+const ArialMultiplex: FunctionComponent<ArialMultiplexProps> = () => {
+	return (
+		<>
+			<ins className="adsbygoogle block" data-ad-format="autorelaxed" data-ad-client="ca-pub-7732822326275292" data-ad-slot="7212757136"></ins>
+			<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+		</>
+	);
+};
+
+export default ArialMultiplex;
