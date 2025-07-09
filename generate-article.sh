e8d0c4145a989fa3e4fa5e1381fb4a12ca2f3a7f
William Donnette <willdonn17@gmail.com>
2025-07-09 12:39:43 +0200

    Ajout d'un nouvel article généré automatiquement

--- generate-article.sh
+++ generate-article.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+ENV MISTRAL_API_KEY=gS75nCSjLGyPsmjldV4WxYyvHVYSOPdK
+touch .env
+echo MISTRAL_API_KEY=$MISTRAL_API_KEY > .env
 # Générer l'article
 export BRANCH_NAME=$(node generate-article.mjs $@| tail -n 1)
 
